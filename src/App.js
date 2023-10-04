bcf3fda2d80379705c5e37607015c202d097f242
andradajus <andradajustinmarcus@gmail.com>
2023-10-04 21:20:19 +0800

    Setup tailwind configs

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div>
-
+      <div className="flex flex-col">Flex Test Column
+      <div className="bg-red-100">1</div>
+      <div className="bg-red-200">2</div>
+      <div className="bg-red-300">3</div>
+      <div className="bg-red-400">4</div>
+      </div>
+    <div>Tailwind Test</div>
 
 
     </div>
